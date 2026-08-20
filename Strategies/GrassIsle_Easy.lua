3fb4126aeb8199b0326f4db30ebb9b858de8b277
DuxiiT <johony.mahfoud06@gmail.com>
2026-08-20 14:48:08 +0200

    Create GrassIsle_Easy.lua

---
+++ Strategies/GrassIsle_Easy.lua
@@ -0,0 +1,102 @@
+TDS:Ready()
+TDS:Place("Scout", 11.774942, 1.000003, -17.127649)
+TDS:Place("Scout", 11.953791, 1.000003, -20.178905)
+TDS:Place("Scout", 12.280043, 1.000004, -23.210201)
+TDS:Place("Scout", 15.27735, 1.000004, -22.473429)
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+
+-- [ Wave 2 ] --
+TDS:Place("Scout", 14.868414, 1.000003, -19.43437)
+TDS:Upgrade(5)
+TDS:Upgrade(4)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(3)
+TDS:Place("Scout", 14.742072, 1.000003, -16.339964)
+TDS:Upgrade(6)
+TDS:Place("Scout", 17.761395, 1.000003, -20.615192)
+
+-- [ Wave 4 ] --
+TDS:Upgrade(7)
+TDS:Place("Scout", 17.645222, 1.000003, -17.590342)
+TDS:Upgrade(8)
+TDS:Place("Scout", 17.137068, 1.000002, -14.512873)
+TDS:Upgrade(9)
+TDS:Place("Scout", 11.568352, 1.000002, -14.093931)
+TDS:Upgrade(10)
+TDS:Place("Scout", 14.398523, 1.000002, -12.748578)
+TDS:Upgrade(11)
+TDS:Place("Scout", 11.690114, 1.000002, -11.082199)
+TDS:Upgrade(12)
+
+-- [ Wave 5 ] --
+TDS:Place("Scout", 9.260077, 1.000004, -23.540365)
+TDS:Upgrade(13)
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+TDS:Upgrade(3)
+
+-- [ Wave 6 ] --
+TDS:Upgrade(13)
+TDS:Upgrade(10)
+TDS:Upgrade(12)
+TDS:Upgrade(11)
+
+-- [ Wave 7 ] --
+TDS:Upgrade(6)
+
+-- [ Wave 8 ] --
+TDS:Upgrade(5)
+TDS:Upgrade(7)
+TDS:Upgrade(4)
+TDS:Upgrade(8)
+TDS:Upgrade(9)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(1)
+TDS:Upgrade(1)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(6)
+
+-- [ Wave 11 ] --
+TDS:Upgrade(6)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(2)
+TDS:Upgrade(2)
+TDS:Upgrade(13)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(13)
+
+-- [ Wave 14 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(3)
+TDS:Upgrade(4)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(8)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+
+-- [ Wave 19 ] --
+TDS:Upgrade(12)
+TDS:Upgrade(12)
